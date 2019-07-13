9e1abc01e87ade6d1d345c8e2d43070e905f1232
doulos76 <inojyes@gmail.com>
2019-07-14 01:42:40 +0900

    add ToDoListWithRealm project and .gitignore

---
+++ TodoListWithRealm/TodoListWithRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoListWithRealm
+//
+//  Created by dave76 on 14/07/2019.
+//  Copyright © 2019 dave76. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
